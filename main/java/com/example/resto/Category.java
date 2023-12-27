b1653d4708650837e0f191384706a0fb8d0c804c
Akshaya <136215517+Akshaya0096@users.noreply.github.com>
2023-12-27 23:08:52 +0530

    Add files via upload

---
+++ main/java/com/example/resto/Category.java
@@ -0,0 +1,13 @@
+package com.example.resto;
+
+public class Category {
+    static String Status;
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public void setStatus(String status) {
+        Status = status;
+    }
+}
